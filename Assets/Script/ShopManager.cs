d1330e26d04c165c2fa74ee39dd5f3e97752f7e4
fatin farhana <f.fatinfarhanamasud@gmail.com>
2023-10-12 23:01:57 +0800

    Canvas Shop
    Created Shop Item Prefab

---
+++ Assets/Script/ShopManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopManager : MonoBehaviour
+{
+    public Text coinsText;
+    public GameObject foodItemPrefab;
+    public Transform shopPanel;
+    public Transform playerInventory;
+
+    private int coins = 100; // Initial player coins
+
+    
+}
